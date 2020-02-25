2d6f4590e3ec4414f9d6a294ef0c8d41d4471824
Washington-bezerra <61431230+Washington-bezerra@users.noreply.github.com>
2020-02-24 21:29:55 -0300

    added new line

--- Exercicio 001.py
+++ Exercicio 001.py
@@ -2,4 +2,4 @@ nome = input('Qual é o seu nome? ')
 print('É um prazer te conhecer, {}!'.format(nome))
 //
 nome = input('Qual é o seu nome? ')
-print('É um prazer te aconhecer, ',nome)
+print('Prazer em te aconhecer, ',nome)
